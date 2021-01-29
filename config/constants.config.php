ad34c0ea84857934c5bf690c1f78b392d5c25360
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-01-29 18:21:24 +0800

    setup project repo (no longer using routers and MVC)

---
+++ config/constants.config.php
@@ -0,0 +1,18 @@
+<?php
+// DB Params for local server
+define('CERRTIFICATE_AUTHORITY_FILE', NULL);
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'test');
+define('DB_PORT', 3306);
+
+// DB Params for production DB
+// define('CERRTIFICATE_AUTHORITY_FILE', '/BaltimoreCyberTrustRoot.crt.pem');
+// define('DB_SERVER', 'darwin-hello-world.mysql.database.azure.com');
+// define('DB_USER', 'darwin3750@darwin-hello-world');
+// define('DB_PASS', 'Darwin2018110237');
+// define('DB_NAME', 'darwindb');
+// define('DB_PORT', 3306);
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php 
-  echo $_SERVER['REQUEST_URI'];
-  echo '<br/>';
-  echo $_GET['url'];
+
+require_once "config/constants.php";
+require_once "components/scripts/database-handler.script.php";
+
+$sql_query_cardcode = "SELECT * FROM test_table";
+$sql_statement = mysqli_stmt_init($conn);
+if(!mysqli_stmt_prepare($sql_statement, $sql_query_cardcode)){
+  echo "sqlerror";
+  exit();
+}
+else{
+  mysqli_stmt_execute($sql_statement);
+    $result = mysqli_stmt_get_result($sql_statement);
+    $matches_cardcode=0;
+    while($row = mysqli_fetch_assoc($result)){
+        echo $row['COLUMN1'];
+    }
+}
+
 ?>
