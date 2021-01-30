9379c5ba8bec6e8ff598fdb4c94547b2de1a8af9
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-01-30 22:34:03 +0800

    give the call to posts table its own script

--- index.php
+++ index.php
@@ -20,21 +20,8 @@ require_once "constants.php";
     <div class="row">
       <?php
       require_once "components/scripts/connect-database.script.php";
-
-      $sql_query_cardcode = "SELECT * FROM POSTS";
-      $sql_statement = mysqli_stmt_init($conn);
-      if (!mysqli_stmt_prepare($sql_statement, $sql_query_cardcode)) {
-        echo "sqlerror";
-      } else {
-        mysqli_stmt_execute($sql_statement);
-        $result = mysqli_stmt_get_result($sql_statement);
-        while ($row = mysqli_fetch_assoc($result)) {
-          $title = $row['TITLE'];
-          $description = $row['DESCRIPTION'];
-          $img_src = $row['IMG_SRC'];
-          include PRODUCT_CARD;
-        }
-      }
+      $template = PRODUCT_CARD;
+      require_once "components/scripts/display-posts.script.php";
       ?>
     </div>
   </section>
