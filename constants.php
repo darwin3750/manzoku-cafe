57e30c6f3fc373c40c04861b4995ae1a3af04d32
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-01-30 15:22:00 +0800

    ignore sec cert and database config

---
+++ constants.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SERVER_ROOT', preg_replace('|\\\|', '/', __DIR__));
+
+require_once SERVER_ROOT . "/config/assets.config.php";
+require_once SERVER_ROOT . "/config/database.config.php";
+
+?>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+require_once "constants.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href=<?php echo CSS; ?>>
+  <title>Manzoku Cafe</title>
+</head>
+
+<body class="">
+  <?php include NAVBAR; ?>
+  <section class="container">
+
+    <h1>MENU</h1>
+    <div class="row">
+      <?php
+      require_once "components/scripts/connect-database.script.php";
+
+      $sql_query_cardcode = "SELECT * FROM POSTS";
+      $sql_statement = mysqli_stmt_init($conn);
+      if (!mysqli_stmt_prepare($sql_statement, $sql_query_cardcode)) {
+        echo "sqlerror";
+      } else {
+        mysqli_stmt_execute($sql_statement);
+        $result = mysqli_stmt_get_result($sql_statement);
+        while ($row = mysqli_fetch_assoc($result)) {
+          $title = $row['TITLE'];
+          $description = $row['DESCRIPTION'];
+          $img_src = $row['IMG_SRC'];
+          include PRODUCT_CARD;
+        }
+      }
+      ?>
+    </div>
+  </section>
+
+
+
+  <?php include FOOTER; ?>
+</body>
+
+</html>
