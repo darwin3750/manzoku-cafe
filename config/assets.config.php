3746f20896ccd7ea0bde0301652de11bedbb1945
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-01-30 14:52:29 +0800

    add product-card template

--- config/assets.config.php
+++ config/assets.config.php
@@ -4,5 +4,6 @@ define('CSS', "/assets/styles/styles.css");
 define('LOGO', "/assets/images/"); 
 define('NAVBAR', SERVER_ROOT . "/components/templates/navbar.tmpl.php"); 
 define('FOOTER', SERVER_ROOT . "/components/templates/footer.tmpl.php"); 
+define('PRODUCT_CARD', SERVER_ROOT . "/components/templates/product-card.tmpl.php"); 
 
 ?>
