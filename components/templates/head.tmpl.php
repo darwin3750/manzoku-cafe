f9bf5ae22f54f0ee6aa939be797f3390191a9bd4
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-01-31 16:50:28 +0800

    fix paths, add sign out, and add admin setup account

--- components/templates/head.tmpl.php
+++ components/templates/head.tmpl.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href=<?php echo CSS; ?>>
-  <link rel="icon" href="<?php echo LOGO2; ?>" type="image/gif" sizes="16x16">
   <title><?php echo SITE_NAME . " | "  . $page_name ?></title>
   <body class="theme1 manzoku-bg-primary">
 </head>
