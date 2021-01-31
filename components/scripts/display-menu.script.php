79f53b6ca6033246e2c120bc792d788d8eea49b0
pvrey <razielalmightyy@gmail.com>
2021-02-01 02:19:34 +0800

    display menu from script

---
+++ components/scripts/display-menu.script.php
@@ -0,0 +1,16 @@
+<?php
+
+$sql_query_cardcode = "SELECT * FROM MENU";
+$sql_statement = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($sql_statement, $sql_query_cardcode)) {
+  echo "sqlerror";
+} else {
+  mysqli_stmt_execute($sql_statement);
+  $result = mysqli_stmt_get_result($sql_statement);
+  while ($row = mysqli_fetch_assoc($result)) {
+    $img_src = $row['IMG_SRC'];
+    $category = $row['CATEGORY'];
+    include $template;
+  }
+}
+      ?>
