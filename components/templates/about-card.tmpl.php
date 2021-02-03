51eaf08f42ee7865feedbb2a1ba5c8c8626ee1e5
pvrey <razielalmightyy@gmail.com>
2021-02-03 17:26:34 +0800

    display cards on about page

--- components/templates/about-card.tmpl.php
+++ components/templates/about-card.tmpl.php
@@ -1,36 +1,26 @@
-<?php
-  $description = strip_tags($description);
-  if (strlen($description) > 160) {
-  
-      // truncate string
-      $stringCut = substr($description, 0, 160);
-      $endPoint = strrpos($stringCut, ' ');
-  
-      //if the string doesn't contain any space then it will cut without word basis.
-      $description = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
-      $description .= '...';
-  } 
-  ?>
-<br/>
 <?php if ($title == "BRINGING JAPANESE CUISINE TO YOUR TASTEBUDS")
-   {
-     echo 
-    '<div class="card bg-dark text-white">
-    <img src="'.$img_src.'" class="card-img">
-    <div class="card-img-overlay">
-        <h5 class="card-title">'.$title.'</h5>
-        <p class="card-text">'.$idescription.'</p>
-    </div>
-    </div>'
+    {
+        echo 
+        '<div class="card" style="width: 95%; margin-left:auto; margin-right:auto;">
+            <div class="card-body" >
+            <h5 class="card-title">'.$title.'</h5>
+            '.$description.'
+            </div>
+        </div>';
+    }
+   else 
+    {
+        echo 
+        '<div class="col-sm-4 d-flex mt-5 mx-auto">
+            <div class="col">
+                <div class="card h-100">
+                <div class="card-body ">
+                    <h5 class="card-title">'.$title.'</h5>
+                    <p class="card-text">'.$description.'</p>
+                </div>
+                </div>
+            </div>
+        </div>';
+    }
 ?>
 
-<div class="col-sm-4 d-flex" style="margin-left:auto; margin-right:auto;">
-  <div class="col">
-    <div class="card h-100">
-      <div class="card-body">
-        <h5 class="card-title"><?php echo $title ?></h5>
-        <p class="card-text"><?php echo $description ?></p>
-      </div>
-    </div>
-  </div>
-</div>
