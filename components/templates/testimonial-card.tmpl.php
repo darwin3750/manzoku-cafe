ad28467a239818c9fb6576c93fcdd9e73e3bb673
pvrey <razielalmightyy@gmail.com>
2021-02-04 02:51:41 +0800

    fix author's image

--- components/templates/testimonial-card.tmpl.php
+++ components/templates/testimonial-card.tmpl.php
@@ -15,7 +15,7 @@
 <div class="col-sm-4 d-flex" style="margin-left:auto; margin-right:auto; ">
 <div class="p-2 row-sm-2 d-flex align-items-stretch manzoku-bg-contrast mb-5">
 <div class="card p-40 text-black manzoku-bg-contrast mb-1" style="width: 20rem; height: 570px; border: none;">
-  <img class="card-img-top img-fluid rounded img-thumbnail" src="<?php echo ROOT_TESTIMONIALS_FOLDER . $img_src ?>"  alt="assets\images\testimonies\default-user.png">
+  <img class="card-img-top rounded" src="<?php echo ROOT_TESTIMONIALS_FOLDER . $img_src ?>"  alt="<?php echo ROOT_TESTIMONIALS_FOLDER?>default-user.png" style="width: 320px; height: 320px;">
   <div class="card-body" style="text-align:center;">
   <p class="card-body" style="margin-top:-8%;"><?php echo $content ?></p>
   <div style="position: absolute; margin-left: auto; margin-right: auto; left: 0; right: 0; text-align: center; bottom: 0; ">
