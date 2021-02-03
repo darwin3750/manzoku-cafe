7fe332bb6f644336df63404fb6a71a6c2e64185a
pvrey <razielalmightyy@gmail.com>
2021-02-04 02:40:58 +0800

    design testimonial cards (bg,text alignment)

--- components/templates/testimonial-card.tmpl.php
+++ components/templates/testimonial-card.tmpl.php
@@ -12,14 +12,14 @@
   } 
   ?>
 <br/>
-<div class="col-sm-4 d-flex" style="margin-left:auto; margin-right:auto;">
-<div class="p-2 row-sm-2 d-flex align-items-stretch">
-<div class="card shadow p-40 text-white bg-secondary mb-4" style="width: 20rem; height: 550px;">
-  <img class="card-img-top img-fluid rounded img-thumbnail" src="assets\images\testimonies\default-user.png"  alt="Card image cap">
+<div class="col-sm-4 d-flex" style="margin-left:auto; margin-right:auto; ">
+<div class="p-2 row-sm-2 d-flex align-items-stretch manzoku-bg-contrast mb-5">
+<div class="card p-40 text-black manzoku-bg-contrast mb-1" style="width: 20rem; height: 570px; border: none;">
+  <img class="card-img-top img-fluid rounded img-thumbnail" src="assets\images\testimonies\default-user.png"  alt="assets\images\testimonies\default-user.png">
   <div class="card-body" style="text-align:center;">
   <p class="card-body" style="margin-top:-8%;"><?php echo $content ?></p>
-  <div style="position: absolute; bottom: 0; left:36%;";>
-  <p class="card-title"><?php echo $author ?></p>
+  <div style="position: absolute; margin-left: auto; margin-right: auto; left: 0; right: 0; text-align: center; bottom: 0; ">
+  <p class="card-title" style="font-style: italic;"><?php echo $author ?></p>
   <p class="card-title" style="font-weight:bold;"><?php echo $title ?></p>
   </div>
   </div>
