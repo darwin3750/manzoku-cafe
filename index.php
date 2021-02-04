f618733d6c7702dbc170db58035999dd89e71ebd
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-04 16:06:10 +0800

    set up carousel

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $page_name = "Home";
 require_once HEAD;
 include NAVBAR; ?>
 
-<section`>
+<section class="manzoku-color-contrast">
   <?php
   //connect to DB
   require_once "components/scripts/connect-database.script.php";
@@ -23,6 +23,28 @@ include NAVBAR; ?>
   //close DB connection
   require_once "components/scripts/close-database.script.php";
   ?>
-</section>
 
+
+
+<div class="carousel"
+  data-flickity='{ "wrapAround": true }'>
+  <div class="carousel-cell">1</div>
+  <div class="carousel-cell">2</div>
+</div>
+
+
+<style>
+
+.carousel-cell {
+  width: 100%;
+  height: 200px;
+  margin-right: 10px;
+  background: #8C8;
+  border-radius: 5px;
+  counter-increment: carousel-cell;
+}
+
+</style>
+
+</section>
 <?php include FOOTER; ?>
