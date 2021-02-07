862aacb44a34af006ea9e8ae438ef50468578bda
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-07 14:34:58 +0800

    fix JS bug on alternate hosting setups

--- components/templates/head.tmpl.php
+++ components/templates/head.tmpl.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href=<?php echo CSS; ?>>
   <title><?php echo SITE_NAME . " | "  . $page_name ?></title>
   <link rel="icon" href="<?php echo LOGO2;?>" type="image/gif" sizes="16x16">
-  <script defer src="../../assets/main.js"></script>
-  <script src="../../assets/flickity.pkgd.js"></script>
+  <script defer src="assets/main.js"></script>
+  <script src="assets/flickity.pkgd.js"></script>
   <body class="theme1 manzoku-bg-primary overflow-auto">
 </head>
