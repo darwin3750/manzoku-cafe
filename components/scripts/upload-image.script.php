ef50c9ae1016d2de39abbc3742306402f65fb8f7
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-08 01:13:59 +0800

    add delete image functionality

--- components/scripts/upload-image.script.php
+++ components/scripts/upload-image.script.php
@@ -1,40 +1,44 @@
 <?php 
 if(isset($_POST['upload-submit'])){
-  echo "asd";
-}
-echo 'hello' . '<br/>';
-echo $_GET['destfolder'] . '<br/>';
 
-require "../../constants.php";
+  require "../../constants.php";
 
-$target_dir = SERVER_ROOT . "/assets/images/" . $_GET['destfolder'] . "/";
-$target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+  $target_dir = SERVER_ROOT . "/assets/images/" . $_GET['destfolder'] . "/";
+  $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
+  $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-// Check if image file is a actual image or fake image
-$output = getimagesize($_FILES["fileUpload"]["tmp_name"]) !== false ? "File is an image" : "File is not an image.";
-echo $output;
+  // Check if image file is a actual image or fake image
+  $output = getimagesize($_FILES["fileUpload"]["tmp_name"]) !== false ? "File is an image" : "File is not an image.";
+  echo $output;
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-}
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+  }
 
-// Check file size
-if ($_FILES["fileUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-}
+  // Check file size
+  if ($_FILES["fileUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+  }
 
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-}
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  }
 
-//
-if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
-  echo "The file ". htmlspecialchars( basename( $_FILES["fileUpload"]["name"])). " has been uploaded.";
-} else {
-  echo "Sorry, there was an error uploading your file.";
+  //
+  if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileUpload"]["name"])). " has been uploaded.";
+    header("Location: ../../config.admin.php?upload=success");
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+    header("Location: ../../config.admin.php?upload=delete");
+  }
+
+  exit();
+}else{
+  header("Location: ../../");
+  exit();
 }
 ?>
--- components/templates/admin-config-files-card.tmpl.php
+++ components/templates/admin-config-files-card.tmpl.php
@@ -1,6 +1,17 @@
 <div class="col-md-3 p-1">
   <div class="p-2 manzoku-border-contrast-1 rounded h-100 d-flex flex-column justify-content-between">
-    <img src="<?php echo TEMPLATE_IMAGES_FOLDER . $folder . "/" . $file ?>" class="img-fluid">
+    <section>
+      <form class="float-right" method="POST" action="components/scripts/delete-image.script.php?dest=<?php echo $folder . "/" . $file?>">
+        <div class="svg-container-icon-1" onclick="this.parentElement.submit();">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g fill='var(--manzoku-contrast)'>
+              <path d="M19.5 4.5c-.8-.8-2.2-.8-3 0L12 9 7.5 4.5c-.8-.8-2.2-.8-3 0-.8.8-.8 2.2 0 3L9 12l-4.5 4.5c-.8.8-.8 2.2 0 3 .8.8 2.2.8 3 0L12 15l4.5 4.5c.8.8 2.2.8 3 0 .8-.8.8-2.2 0-3L15 12l4.5-4.5c.8-.8.8-2.2 0-3z"></path>
+            </g>
+          </svg>
+        </div>
+      </form>
+      <img src="<?php echo TEMPLATE_IMAGES_FOLDER . $folder . "/" . $file ?>" class="img-fluid">
+    </section>
     <div class="d-flex align-items-start">
       <p><?php echo $file ?></p>
     </div>
