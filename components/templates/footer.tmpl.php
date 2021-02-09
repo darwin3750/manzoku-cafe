01a5dfe45477be86d8ae3c04bd191fc7c5bc0428
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-09 22:23:53 +0800

    redo footer

--- components/templates/footer.tmpl.php
+++ components/templates/footer.tmpl.php
@@ -1,17 +1,71 @@
-<section class="modal-footer" style="background-image: linear-gradient(to right, #383838, #8c8c8c);">
-  <div class="row" style="padding:10px">
-    <div class="col-sm-2" style="padding-left:20px">
-      <img src=<?php echo LOGO ?> width="55" style="padding-left:10px; padding-top: 20px"></br>
-    </div>
-    <div class="col-sm-2 text-left" style="padding:20px;">
-      <a class="manzoku-font-1" style="padding-left:10px" href="index.php">Home</a></br>
-      <a class="manzoku-font-1" style="padding-left:10px" href="about_us.php">About Us</a></br>
-      <a class="manzoku-font-1" style="padding-left:10px" href="contact_us.php">Contact Us</a></br>
-      <a class="manzoku-font-1" style="padding-left:10px" href="testimonies.php">Testimonies</a></br>
-    </div>
-    <div class="col-sm-8" style="padding-right:30px; text-align:left; color:white;">
-      <p class="manzoku-font-1" style="padding-top:20px">Established in 2019, Manzoku Cafe has been serving Japanese cuisines in the City of Cabanatuan. Eventually, their dishes have evolved into a mixture of Japanese-Filipino taste.</p>
-      <p class="manzoku-font-1"><b>© MANZOKU CAFE 2021</b></p>
-    </div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="var(--manzoku-accent1-pos)" fill-opacity="1" d="M0,224L60,224C120,224,240,224,360,229.3C480,235,600,245,720,256C840,267,960,277,1080,272C1200,267,1320,245,1380,234.7L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+</svg>
+
+<footer class="manzoku-bg-accent1-pos pb-5">
+  <div class="container manzoku-color-contrast manzoku-typeface-main0">
+    <section class="row">
+      <div class="col-xl-3"></div>
+      <div class="col-xl-6">
+        <a href="/"><img class="img-fluid p-md-5 p-3" src="assets/images/decorations/japan.svg"></a>
+      </div>
+      <div class="col-xl-3"></div>
+    </section>
+    <section class="row justify-content-center">
+      <div class="col-xl-9 text-left">
+        <section>
+          <header class="">
+            <h3 class="manzoku-color-accent1-neg">Established in 2019,</h3>
+          </header>
+          <article>
+            <p class="p-3 manzoku-font-1 line-height-3 manzoku-black">
+              Manzoku Cafe has been serving Japanese cuisines in the City of Cabanatuan. Eventually, their dishes have evolved into a mixture of Japanese-Filipino taste.
+            </p>
+          </article>
+        </section>
+      </div>
+      <div class="col-xl-3">
+        <h3 class="manzoku-color-accent1-neg">Connect with us: </h3>
+        <section id="footer-socials" class="d-flex justify-content-start">
+          <a class="svg-container-icon-5 manzoku-border-contrast-3 rounded m-1" href="facebook.com">
+            <svg xmlns="http://www.w3.org/2000/svg" aria-label="Facebook" role="img" viewBox="0 0 512 512">
+              <rect width="512" height="512" rx="15%" fill="var(--manzoku-accent1-pos)" />
+              <path id="footer-fb-svg-innershape" d="M355.6 330l11.4-74h-71v-48c0-20.2 9.9-40 41.7-40H370v-63s-29.3-5-57.3-5c-58.5 0-96.7 35.4-96.7 99.6V256h-65v74h65v182h80V330h59.6z" fill="var(--manzoku-contrast)" />
+            </svg>
+          </a>
+          <a class="svg-container-icon-5 manzoku-border-contrast-3 rounded m-1" href="facebook.com">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-label="Instagram" role="img" viewBox="0 0 512 512">
+              <rect width="512" height="512" fill="var(--manzoku-accent1-pos)" />
+              <g id="footer-ig-svg-innershape" fill="none" stroke="var(--manzoku-contrast)" stroke-width="30">
+                <rect width="308" height="308" x="102" y="102" rx="81" />
+                <circle cx="256" cy="256" r="72" />
+                <circle cx="347" cy="165" r="6" />
+              </g>
+            </svg>
+          </a>
+        </section>
+      </div>
+    </section>
+    <hr class="p-1 manzoku-bg-accent2 rounded" />
+    <p class="text-center manzoku-font-2 m-2">© IT135-8L--GROUP BOTSWANA / MANZOKU CAFE 2021</p>
+    <p class="manzoku-bold text-center manzoku-font-1 m-2">Made with love (and the help of <a class=" manzoku-color-base" href="https://www.svgbackgrounds.com/">#SVGBackgrounds</a>).</p>
   </div>
-</section>
+</footer>
+
+
+<style>
+  #footer-socials>.svg-container-icon-5:hover {
+    border: 5px solid var(--manzoku-accent2);
+    transition: 0.4s;
+  }
+
+  #footer-socials>.svg-container-icon-5:hover #footer-fb-svg-innershape {
+    fill: var(--manzoku-base);
+    transition: 0.4s;
+  }
+
+  #footer-socials>.svg-container-icon-5:hover #footer-ig-svg-innershape {
+    stroke: var(--manzoku-base);
+    transition: 0.4s;
+  }
+</style>
