ef96ca6e7258c6f4a8a1085c262ba501976fc4b2
marinZXC <mac24.marin@gmail.com>
2021-02-10 17:43:40 +0800

    update the design of contact us page

--- components/templates/contact-us-card.tmpl.php
+++ components/templates/contact-us-card.tmpl.php
@@ -17,10 +17,16 @@
    {
     echo
     '<div class="col-sm-4" style="background-color:none;">
-    <div class="card manzoku-bg-contrast p-2 m-1">
-      <div class="card-title text-center"><b>'.$title.'</b></div>
-      <div class="text-center">'.$content.'</div>
+    <div class="manzoku-border-base-3 contact-card p-2 m-1">
+      <div class="card-title text-center manzoku-color-secondary"><b>'.$title.'</b></div>
+      <div class="text-center manzoku-color-contrast">'.$content.'</div>
     </div>
     </div>';
    }
 ?>
+<style>
+  .contact-card:hover{
+    border: 3px solid var(--manzoku-accent2);
+    transition: 0.2s;
+  }
+</style>
