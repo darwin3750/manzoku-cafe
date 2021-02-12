70cc37712b22ba5a6d837a88b7f87b9e32ef2416
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-12 14:37:17 +0800

    style view-stats cards

--- components/scripts/display-view-stats.script.php
+++ components/scripts/display-view-stats.script.php
@@ -2,13 +2,8 @@
 
 $sql_query = "SELECT * FROM VIEW_STATS WHERE DATE = CURRENT_DATE";
 if($result = mysqli_query($conn, $sql_query)){
-  $row = mysqli_fetch_assoc($result);
-  $date = $row['DATE'];
-  $home = $row['HOME'];
-  $menus = $row['MENUS'];
-  $about_us = $row['ABOUT_US'];
-  $contact_us = $row['CONTACT_US'];
-  $testimonies = $row['TESTIMONIES'];
+  $values = mysqli_fetch_array($result, 2);
+  $labels = array("Date today", "Home", "Menus", "About Us", "Contact Us", "Testimonies");
   include $template;
 }
 ?>
--- components/templates/admin-view-stats-card.tmpl.php
+++ components/templates/admin-view-stats-card.tmpl.php
@@ -1,13 +1,21 @@
-<h2>Date today:</h2>
-<h5><?php echo $date?></h5>
+<section class="row p-5">
+  <?php foreach ($values as $index => $value) { ?>
+    <div class="col-md-3 p-1">
+      <section class="p-3 manzoku-border-contrast-2 rounded view-stats-card">
+        <h5><?php echo $labels[$index] ?>: </h5>
+        <hr class="manzoku-semibold manzoku-bg-contrast" />
+        <h2 class="manzoku-black"><?php echo $value ?></h2>
+      </section>
+    </div>
+  <?php } ?>
+</section>
 
-<h2>Home:</h2>
-<h5><?php echo $home?></h5>
-<h2>Menus:</h2>
-<h5><?php echo $menus?></h5>
-<h2>About Us:</h2>
-<h5><?php echo $about_us?></h5>
-<h2>Contact Us:</h2>
-<h5><?php echo $contact_us?></h5>
-<h2>Testimonies:</h2>
-<h5><?php echo $testimonies?></h5>
+<style>
+  .view-stats-card{
+    transition: 0.4s;
+  }
+  .view-stats-card:hover{
+    border: 2px solid var(--manzoku-accent2);
+    color: var(--manzoku-base);
+  }
+</style>
