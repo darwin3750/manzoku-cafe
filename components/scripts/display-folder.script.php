d822b2af4818a8a024ada172b8594d81f52c924a
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-15 20:24:14 +0800

    make display folder regex case insensitive

--- components/scripts/display-folder.script.php
+++ components/scripts/display-folder.script.php
@@ -1,6 +1,6 @@
 <?php 
 foreach (scandir(IMAGES_FOLDER . $folder . "/") as $key => $file) {
-  if(preg_match("/(jpeg|jpg|png|webp)$/", $file)){
+  if(preg_match("/(jpeg|jpg|png|webp)$/i", $file)){
     include ADMIN_CONFIG_FILES_CARD;
   }
 }
