cd39bdfe99d455beaf1491d2f76f290e55012f0c
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-02-15 20:22:53 +0800

    add strtolower fulter to image upload

--- components/scripts/upload-image.script.php
+++ components/scripts/upload-image.script.php
@@ -22,6 +22,7 @@ if(isset($_POST['upload-submit'])){
   }
 
   // Allow certain file formats
+  $imageFileType = strtolower($imageFileType);
   if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
   && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
